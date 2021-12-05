3fde8da9868d981de3728a8c14dcb8b0b67ec277
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-12-05 08:44:59 +0000

    feat: ひとまずSlack Bolt Appを起動できる環境を構築
    deno run --no-check=remote --allow-read --allow-env --allow-net src/app.ts

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import "https://deno.land/x/dotenv@v2.0.0/load.ts"
+import { App } from "https://deno.land/x/slack_bolt@1.0.0/mod.ts"
+
+const app = new App({
+    signingSecret: Deno.env.get("SLACK_SIGNING_SECRET"),
+    token: Deno.env.get("SLACK_BOT_TOKEN"),
+    ignoreSelf: true,
+})
+
+app.event("message", async ({ event, say }) => {
+    console.log(event)
+    await say("pong")
+})
+
+await app.start({ port: 3000 })
+console.log("🦕 ⚡️")
