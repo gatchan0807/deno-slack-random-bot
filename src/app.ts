d599a4047ac08ffa7812606065a8f2a6f18dfbaa
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-12-06 17:17:31 +0000

    chore: deno fmtの実行

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import "https://deno.land/x/dotenv@v2.0.0/load.ts"
-import { App } from "https://deno.land/x/slack_bolt@1.0.0/mod.ts"
+import "https://deno.land/x/dotenv@v2.0.0/load.ts";
+import { App } from "https://deno.land/x/slack_bolt@1.0.0/mod.ts";
 
 const app = new App({
-    signingSecret: Deno.env.get("SLACK_SIGNING_SECRET"),
-    token: Deno.env.get("SLACK_BOT_TOKEN"),
-    ignoreSelf: true,
-})
+  token: Deno.env.get("SLACK_BOT_TOKEN"),
+  signingSecret: Deno.env.get("SLACK_SIGNING_SECRET"),
+});
 
 app.event("message", async ({ event, say }) => {
-    console.log(event)
-    await say("pong")
-})
+  console.log(event);
+  await say("pong");
+});
 
-await app.start({ port: 3000 })
-console.log("🦕 ⚡️")
+await app.start({ port: 3000 });
+console.log("🦕 ⚡️");
