5711945fc7029044be4c9c9b76e2243cd298342d
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-12-08 16:20:56 +0000

    fix: slack_boltの依存先で Deno.version.deno を呼び出そうとして落ちるので自前で定義してみる

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,5 @@
+Deno.version.deno = "1.16.4";
+
 export { App } from "https://deno.land/x/slack_bolt@1.0.0/mod.ts";
 export { initializeApp } from "https://cdn.skypack.dev/@firebase/app@v0.7.10?dts";
 export * from "https://cdn.skypack.dev/@firebase/firestore@v3.4.0?dts";
