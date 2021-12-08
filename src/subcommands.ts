002c6281969f506d03c1002db2d84699bd1b8089
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-12-08 04:08:24 +0000

    feat: 開発時に必要なpingコマンドとグループの解散サブコマンドの追加

--- src/subcommands.ts
+++ src/subcommands.ts
@@ -1,8 +1,12 @@
 const botName = "U02PXKJA27K";
 
 export const SubCommandPattern = {
+  ping: new RegExp(`^\<\@${botName}\> ping.*`),
   add: new RegExp(`^\<\@${botName}\> add.*`),
   delete: new RegExp(`^\<\@${botName}\> (delete|remove).*`),
   create: new RegExp(`^\<\@${botName}\> create.*`),
+  disband: new RegExp(
+    `^\<\@${botName}\> (disband|delete-group|remove-group).*`,
+  ),
   randomSort: new RegExp(`^\<\@${botName}\> random\-sort.*`),
 };
