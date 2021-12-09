0c651ca7ff48b7408b1e0d719fca73af2282a1a0
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-12-09 02:11:59 +0000

    fix: ローカル環境化でparseエラーになるので三項演算子で判定するように変更

--- src/firestore.ts
+++ src/firestore.ts
@@ -1,7 +1,9 @@
 import { getFirestore, initializeApp } from "./deps.ts";
 
 const firebaseConfig = JSON.parse(Deno.env.get("FIREBASE_CONFIG") ?? "");
-const reParsedFirebaseConfig = JSON.parse(firebaseConfig);
+const reParsedFirebaseConfig = typeof firebaseConfig === "string"
+  ? JSON.parse(firebaseConfig)
+  : firebaseConfig;
 
 const firebaseApp = initializeApp(reParsedFirebaseConfig);
 export const db = getFirestore(firebaseApp);
