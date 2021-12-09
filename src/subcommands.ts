fd21faf9870b14a0f1dc12d9e6f408ee29591db6
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-12-09 02:42:42 +0000

    feat: コマンドヘルプの内容が意外と読みにくかったので絵文字を使う形に変更

--- src/subcommands.ts
+++ src/subcommands.ts
@@ -18,14 +18,14 @@ export const helpMessage = `使い方の説明ッ！
 ⚠ コピペすると、コマンドに見えないスペースが入ってしまって無視される場合があるのでお気をつけください🙏
 
 ~~~~~~~ 便利コマンド系 ~~~~~~~
-@/random-bot (help|h) # => ヘルプの表示
-@/random-bot ping # => 起動確認（バックエンドがCloud Runのため、使われていないと寝ていることがあるので先にこれを叩いておくとレスポンスが早く返って来やすくなります）
+@/random-bot (help|h) 👉 ヘルプの表示
+@/random-bot ping 👉 起動確認（バックエンドがCloud Runのため、使われていないと寝ていることがあるので先にこれを叩いておくとレスポンスが早く返って来やすくなります）
 ~~~~~~~ データ登録・削除系 ~~~~~~~
-@/random-bot create [group-name] # => グループ（メンバーを追加する枠）の作成
-@/random-bot (disband|delete-group|remove-group) [group-name] # => グループ（メンバーを追加する枠）の解散
-@/random-bot list [group-name] # => グループ内のメンバー一覧の表示
-@/random-bot add [group-name] [member-name] # => グループにメンバーの追加（メンバー名はテキストもメンションもどちらも登録可能です）
-@/random-bot (delete|remove) [group-name] [member-name] # => グループからメンバーの削除
+@/random-bot create [group-name] 👉 グループ（メンバーを追加する枠）の作成
+@/random-bot (disband|delete-group|remove-group) [group-name] 👉 グループ（メンバーを追加する枠）の解散
+@/random-bot list [group-name] 👉 グループ内のメンバー一覧の表示
+@/random-bot add [group-name] [member-name] 👉 グループにメンバーの追加（メンバー名はテキストもメンションもどちらも登録可能です）
+@/random-bot (delete|remove) [group-name] [member-name] 👉 グループからメンバーの削除
 ~~~~~~~ シャッフル実行系 ~~~~~~~
-@/random-bot random-sort [group-name] # => グループ内のメンバー一覧をシャッフルして並び替え
+@/random-bot random-sort [group-name] 👉 グループ内のメンバー一覧をシャッフルして並び替え
 `;
