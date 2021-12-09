6e431d1f6cdeacbdc0bbfb9d157d3035395d9a98
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-12-09 01:47:38 +0000

    chore: firebaseConfigがまだStringのままなのではないか説を検証

--- src/firestore.ts
+++ src/firestore.ts
@@ -1,11 +1,15 @@
 import { getFirestore, initializeApp } from "./deps.ts";
 
 const firebaseConfig = JSON.parse(Deno.env.get("FIREBASE_CONFIG") ?? "");
+const reParsedFirebaseConfig = JSON.parse(firebaseConfig);
 console.info(firebaseConfig);
-console.info(firebaseConfig["projectId"]);
-console.info(firebaseConfig.projectId);
-console.info(firebaseConfig["appId"]);
-console.info(firebaseConfig.appId);
+console.info(reParsedFirebaseConfig);
+console.info(typeof firebaseConfig);
+console.info(typeof reParsedFirebaseConfig);
+console.info(reParsedFirebaseConfig["projectId"]);
+console.info(reParsedFirebaseConfig.projectId);
+console.info(reParsedFirebaseConfig["appId"]);
+console.info(reParsedFirebaseConfig.appId);
 
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
