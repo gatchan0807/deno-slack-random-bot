d7e2604cfd88ce5d714b91e24399fc2792d35210
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-12-09 01:42:08 +0000

    chore: プロパティの取得方法によってundefinedになる理由を調査中

--- src/firestore.ts
+++ src/firestore.ts
@@ -3,8 +3,9 @@ import { getFirestore, initializeApp } from "./deps.ts";
 const firebaseConfig = JSON.parse(Deno.env.get("FIREBASE_CONFIG") ?? "");
 console.info(firebaseConfig);
 console.info(firebaseConfig["projectId"]);
-console.info(initializeApp);
+console.info(firebaseConfig.projectId);
+console.info(firebaseConfig["appId"]);
+console.info(firebaseConfig.appId);
 
 const firebaseApp = initializeApp(firebaseConfig);
-console.info(firebaseApp);
 export const db = getFirestore(firebaseApp);
