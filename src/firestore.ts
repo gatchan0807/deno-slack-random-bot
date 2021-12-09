9b704434a51bdc81ef75e7936daa7bf64f22493a
gatchan0807 <yuki.dolphin.koga@gmail.com>
2021-12-09 09:13:15 +0900

    chore: なぜかCloud RunでFirebase設定を受け取れていないので出力させてみる

--- src/firestore.ts
+++ src/firestore.ts
@@ -1,5 +1,7 @@
 import { getFirestore, initializeApp } from "./deps.ts";
 
 const firebaseConfig = JSON.parse(Deno.env.get("FIREBASE_CONFIG") ?? "");
+console.info(firebaseConfig);
+
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
