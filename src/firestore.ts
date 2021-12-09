cc84ac8a3b4794647848bdf93d591d0fbc1bd2a1
gatchan0807 <yuki.dolphin.koga@gmail.com>
2021-12-09 10:34:54 +0900

    chore: なぜinitializeAppがprojectIdを受け取ることができていないのか調査

--- src/firestore.ts
+++ src/firestore.ts
@@ -2,6 +2,9 @@ import { getFirestore, initializeApp } from "./deps.ts";
 
 const firebaseConfig = JSON.parse(Deno.env.get("FIREBASE_CONFIG") ?? "");
 console.info(firebaseConfig);
+console.info(firebaseConfig["projectId"]);
+console.info(initializeApp);
 
 const firebaseApp = initializeApp(firebaseConfig);
+console.info(firebaseApp);
 export const db = getFirestore(firebaseApp);
