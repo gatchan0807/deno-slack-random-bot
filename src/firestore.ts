ca54b99789d3292df5728026e6ebfea5f8d2c95f
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-12-09 02:10:10 +0000

    fix: Cloud Run環境下でFirebase設定オブジェクトの再パースが必要だったので処理を追加

--- src/firestore.ts
+++ src/firestore.ts
@@ -2,14 +2,6 @@ import { getFirestore, initializeApp } from "./deps.ts";
 
 const firebaseConfig = JSON.parse(Deno.env.get("FIREBASE_CONFIG") ?? "");
 const reParsedFirebaseConfig = JSON.parse(firebaseConfig);
-console.info(firebaseConfig);
-console.info(reParsedFirebaseConfig);
-console.info(typeof firebaseConfig);
-console.info(typeof reParsedFirebaseConfig);
-console.info(reParsedFirebaseConfig["projectId"]);
-console.info(reParsedFirebaseConfig.projectId);
-console.info(reParsedFirebaseConfig["appId"]);
-console.info(reParsedFirebaseConfig.appId);
 
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(reParsedFirebaseConfig);
 export const db = getFirestore(firebaseApp);
